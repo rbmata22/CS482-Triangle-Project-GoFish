21957f5d5859850a302bed8bfaa6a3e72939f2b1
rbmata22 <ry.mata22@gmail.com>
2024-09-23 15:58:22 -0400

    Added Halloween.java and my favorite halloween costume

--- Halloween.java
+++ Halloween.java
@@ -1,3 +1,5 @@
-public void main(String[] args) {
-  System.out.println("Ryland's all-time favorite halloween costume is Clark Kent.\n");
+public class Halloween {
+  public static void main(String[] args) {
+    System.out.println("Ryland's all-time favorite halloween costume is Clark Kent.\n");
+  }
 }
