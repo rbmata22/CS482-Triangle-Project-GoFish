8b6e70f4c44b23c670c69fd916437314c52a2dfb
SGreen24 <113150613+SGreen24@users.noreply.github.com>
2024-09-23 18:00:24 -0400

    Silas fav Halloween Costume

--- Halloween.java
+++ Halloween.java
@@ -1,5 +1,9 @@
 public class Halloween {
   public static void main(String[] args) {
     System.out.println("Ryland's all-time favorite halloween costume is Clark Kent.\n");
+
+    // Silas fav Halloween Costume
+    System.out.println("Silas' favorite Halloween costume of all-time is Miles Morales (Spider-Man)!\n");
+
   }
 }
