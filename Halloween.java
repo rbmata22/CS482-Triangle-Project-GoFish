12fa7e7ad3620187b684ef1d943ce872ca9422fb
cwmorrone <70601877+cwmorrone@users.noreply.github.com>
2024-09-25 08:41:13 -0400

    Update Halloween.java

--- Halloween.java
+++ Halloween.java
@@ -7,6 +7,7 @@ public class Halloween {
     System.out.println("Silas' favorite Halloween costume of all-time is Miles Morales (Spider-Man)!\n");
     // Marley's favorite Halloween Costume
     System.out.println("Marley's favorite Halloween costume of all-time is Batman!!!!!\n");
-
+    // Chases favorite halloween Costume
+    System.out.println("Chase's favorite halloween costume of all time is a T-rex.");
   }
 }
